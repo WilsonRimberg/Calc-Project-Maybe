e4c2461c723bd473d59ced111b5126208a8ec96c
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-11-18 12:08:07 -0500

    Updated from Brython Server: 11/18/2016 12:08:06 PM

--- Wub.py
+++ Wub.py
@@ -1,6 +1,8 @@
+import math
+def func(x,f,h)
+    return (f(x+h)-f(x-h))/(2*h)
 opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
-
-while opening not in ["q"]:
+while opening not in ["q", "z"]:
     if opening not in ["h", "f", "z"]:
         opening=input("It seems you entered an incorrect command, please try again. Enter e to encrypt, d to decrypt, or q to quit: ")
     if opening=="h":
@@ -8,7 +10,7 @@ while opening not in ["q"]:
         if helping=="r":
             opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
         if helping =="f":
-            func=input("A function should be entered in the manner of '3x^2+2x+8'. Press any button to return to the original screen.")
+            func=input("A function should be entered in the manner of '3(x)^2+2(x)+8'. Press any button to return to the original screen.")
             opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
         if helping=="b":
             bound=input("For boundaries, input the lower boundary first, such as '-5', then input the upper boundary, such as '5'. Doing this in the incorrect order will cause the program to fail. Press 'return' to return to the opening screen.") 
@@ -17,11 +19,10 @@ while opening not in ["q"]:
            helping=input("It seems you entered an incorrect command, please try again. Press 'f' for how to enter a function, press 'b' for how to enter boundaries, or press 'r' to return to the previous screen. ")
     if opening=="f":
         function=input("f(x)= ")
-        interval=input("What are the boundaries of the interval? ")
-        print("wub")
-        opening("z")
+        lwrbound=float(input("What is the lower boundary of the function? "))
+        upprbound=float(input("What is the upper boundary of the function? "))
+        opening="z"
 if opening== "q":
     print("Goodbye!")
-if opening=="z":
+if opening== "z":
     print(" ")
-interval=input("What are the boundaries of the interval? ")
