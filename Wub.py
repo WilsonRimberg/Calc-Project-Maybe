116727a532caf55868b449ad890eecd5864ba299
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-11-18 11:36:01 -0500

    Updated from Brython Server: 11/18/2016 11:36:00 AM

--- Wub.py
+++ Wub.py
@@ -1,7 +1,7 @@
 opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
 
 while opening not in ["q"]:
-    if opening not in ["h", "f"]:
+    if opening not in ["h", "f", "z"]:
         opening=input("It seems you entered an incorrect command, please try again. Enter e to encrypt, d to decrypt, or q to quit: ")
     if opening=="h":
         helping=input("What do you need help with? Press 'f' for how to enter a function, press 'b' for how to enter boundaries, or press 'r' to return to the previous screen. ")
@@ -19,6 +19,9 @@ while opening not in ["q"]:
         function=input("f(x)= ")
         interval=input("What are the boundaries of the interval? ")
         print("wub")
+        opening("z")
 if opening== "q":
     print("Goodbye!")
+if opening=="z":
+    print(" ")
 interval=input("What are the boundaries of the interval? ")
