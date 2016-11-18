2f348e8a78c429288d06f497685811dfa2e925cb
WilsonRimberg <wilsonrimberg@hanovernorwichschools.org>
2016-11-18 09:56:58 -0500

    Update Wub.py

--- Wub.py
+++ Wub.py
@@ -1,1 +1,17 @@
+opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
 
+while opening not in ["q"]:
+    if opening not in ["h", "f"]:
+        opening=input("It seems you entered an incorrect command, please try again. Enter e to encrypt, d to decrypt, or q to quit: ")
+    if opening=="h":
+        helping=input("What do you need help with? Press 'f' for how to enter a function, press 'b' for how to enter boundaries, or press 'r' to return to the previous screen. ")
+        if helping=="r":
+            opening= input("Welcome to our Calculus program. Press 'h' for help, press 'f' to insert a function, or press 'q' to quit. ") 
+        if helping =="f"
+            input==
+            
+if opening== "q":
+    print("Goodbye!")
+
+#function=input("f(x)= ")
+#interval=input("What are the boundaries of the interval? ")
